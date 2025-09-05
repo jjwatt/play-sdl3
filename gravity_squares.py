4491bdb1dc837c5bce9778e00751b175b0774ff3
Jesse Wattenbarger <jwatt@broken.watch>
2025-09-05 16:46:36 -0400

    Start on Python version of gravity squares

---
+++ gravity_squares.py
@@ -0,0 +1,14 @@
+import ctypes
+import sdl3
+
+
+class Square:
+    """A Square for moving around."""
+
+
+@sdl3.SDL_main_func
+def main(
+    argc: ctypes.c_int,
+    argv: sdl3.LP_c_char_p
+) -> ctypes.c_int:
+    pass
